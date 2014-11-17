a7e3264773a3d94cea282f87b409357a83401a59
ShrykeWindgrace <shryke.windgrace@gmail.com>
2014-11-17 18:38:50 +0100

    new class to englobe all project data (replaces entries)

---
+++ BLSC/Main/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLSC
+{
+    [Serializable]
+    public class Project
+    {
+        public Entry[] entries;
+        public Project()
+        {
+            entries = new Entry[Enum.GetNames(typeof(EEType)).Length];
+            foreach (EEType eet in Enum.GetValues(typeof(EEType)))
+            {
+                entries[(int)eet] = new Entry(eet);
+            }
+        }
+    }
+}
